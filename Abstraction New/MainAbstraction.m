00545846ba16408aaff6d0a77873a43b0c22b380
Jawad Tayyub <sc12jbmt@cslin134.csunix.comp.leeds.ac.uk>
2017-09-26 14:13:30 +0100

    BIg commit 2

--- Abstraction New/MainAbstraction.m
+++ Abstraction New/MainAbstraction.m
@@ -17,6 +17,8 @@ videoNames = GetVideoNames('Dataset/VideosNames');
 topActNames = unique(videoNames);
 models = cell(size(topActNames,1),1); % full models per top level activities for each uniqu
 
+
+allVidData = {};% concatenated array of all trees for parent mathcing 
 for f = 3 : size(files,1)
     vidFileName = files(f).name;
     disp(['Abstraction-' vidFileName]);
@@ -27,19 +29,24 @@ for f = 3 : size(files,1)
     
     load([path2Hierarchies '/' files(f).name]); % loads the IdsTree and LabelsTree for that video
     
+    allVidData = [allVidData;LabelsTree{1}];
     
+    %%% Merge same parents together %%% LOOK FOR SAME PARENTS EXCEPT ROOT
+    
+    parentNodes = 
+        
     %%% build the exhaustive model %%%% Find the similar nodes and merge
     %%% their children
     
     
-    if isempty(models{modelNum})
-        updatedModel = LabelsTree; % assign the first instant to the model if its empty 
-    else
-        % Augment model with current tree instant
-        updatedModel = augmentModel(models{modelNum},LabelsTree{1}); 
-    end
-    
-    models(modelNum) = updatedModel;
+%     if isempty(models{modelNum})
+%         updatedModel = LabelsTree; % assign the first instant to the model if its empty 
+%     else
+%         % Augment model with current tree instant
+%         updatedModel = augmentModel(models{modelNum},LabelsTree{1}); 
+%     end
+%     
+%     models(modelNum) = updatedModel;
     
 end
 
---
+++ Generally Needed Functions/generateNodeSimilarityMatrix.m
@@ -0,0 +1,17 @@
+function [ mat, nodes ] = generateNodeSimilarityMatrix( nodes )
+%GENERATENODESIMILARITYMATRIX Given a list of nodes, generate a node
+%similarity matrix and return the matrix along with the list
+
+mat = zeros(length(nodes),length(nodes));
+mat(logical(eye(length(mat)))) = 1;
+
+for i = 1 :size(nodes,1)
+    for j = i+1 :size(nodes,1)-1
+        mat(i,j) = getNodeSimilarity(nodes{i},nodes{j});
+    end
+end
+    
+mat = mat + triu(mat)';    
+
+end
+
